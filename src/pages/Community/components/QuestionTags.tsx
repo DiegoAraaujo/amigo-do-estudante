d840a0b850a2481e47b907ea32d0653ada1eb716
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 15:58:43 -0300

    feat(QuestionTags): create component for tags associated with questions

---
+++ src/pages/Community/components/QuestionTags.tsx
@@ -0,0 +1,9 @@
+const QuestionTags = () => {
+  return (
+    <p className="text-primary-dark rounded-2xl bg-purple-700 px-6 py-1 text-xs">
+      forças
+    </p>
+  );
+};
+
+export default QuestionTags;
