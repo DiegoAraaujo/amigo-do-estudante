e47c8dbc84d4f60a7b6cf727d9be2e4a66932a7e
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 17:07:09 -0300

    feat(SummaryPanel): create summary panel displaying daily and total session counts with dark mode support

---
+++ src/pages/Pomodoro/components/SummaryPanel.tsx
@@ -0,0 +1,37 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+const SummaryPanel = () => {
+  const { darkTheme } = useDarkTheme();
+
+  return (
+    <div
+      className={`${darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"} flex-1 rounded-xl p-6`}
+    >
+      <p
+        className={`${darkTheme ? "text-primary-dark" : "text-primary-light"} text-xl sm:text-2xl font-semibold`}
+      >
+        Resumo
+      </p>
+      <div className="grid grid-cols-2 p-6">
+        <div className="flex flex-col items-center">
+          <p className="text-3xl font-bold text-blue-600">0</p>
+          <p
+            className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-xs`}
+          >
+            Hoje
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-3xl font-bold text-green-500">5</p>
+          <p
+            className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-xs`}
+          >
+            Dias
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SummaryPanel;
