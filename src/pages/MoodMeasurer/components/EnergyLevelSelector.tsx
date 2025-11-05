b079035d27c3f00ea6319d7a789b54e4112228eb
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 16:42:06 -0300

    feat(EnergyLevelSelector): create component to select energy level

---
+++ src/pages/MoodMeasurer/components/EnergyLevelSelector.tsx
@@ -0,0 +1,49 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+interface EnergyLevelSelectorProps {
+  energyLevel: number;
+  setEnergyLevel: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const EnergyLevelSelector = ({
+  energyLevel,
+  setEnergyLevel,
+}: EnergyLevelSelectorProps) => {
+  const { darkTheme } = useDarkTheme();
+
+  return (
+    <div className="mb-6 flex flex-col">
+      <h2
+        className={`mb-3 text-sm font-medium ${darkTheme ? "text-primary-dark" : "text-primary-light"}`}
+      >
+        Nivel de Energia {energyLevel}/10
+      </h2>
+      <div>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={energyLevel}
+          className="w-full accent-blue-500"
+          onChange={(e) => {
+            setEnergyLevel(Number(e.target.value));
+          }}
+        />
+        <div className="flex justify-between">
+          <p
+            className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-sm`}
+          >
+            Muito baixa
+          </p>
+          <p
+            className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-sm`}
+          >
+            Muito alta
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EnergyLevelSelector;
