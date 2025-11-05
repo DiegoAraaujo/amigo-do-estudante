3edabf3ed345e4419279cc747f4e49d8535913ee
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 16:45:38 -0300

    feat(MoodRegisterForm): implement mood registration form with mood, energy, factors, and notes inputs; add related TypeScript interfaces

---
+++ src/pages/MoodMeasurer/components/MoodRegisterForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import type { MoodOption } from "../interfaces";
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+import type { Factor } from "../interfaces";
+import EnergyLevelSelector from "./EnergyLevelSelector";
+import FactorsSelection from "./FactorsSelection";
+import MoodSelection from "./MoodSelection";
+import PersonalNotes from "./PersonalNotes";
+
+const MoodRegisterForm = () => {
+  const { darkTheme } = useDarkTheme();
+  const [selectedMood, setSelectedMood] = useState<MoodOption | null>(null);
+  const [selectedFactors, setSelectedFactors] = useState<Factor[]>([]);
+  const [energyLevel, setEnergyLevel] = useState(5);
+  const [personalNotes, setPersonalNotes] = useState("");
+
+  return (
+    <section>
+      <div
+        className={`w-full p-6 ${
+          darkTheme ? "bg-surface-dark" : "bg-surface-light"
+        } rounded-xl`}
+      >
+        <div className="mb-3">
+          <h1
+            className={`text-lg font-semibold sm:text-2xl ${
+              darkTheme ? "text-primary-dark" : "text-primary-light"
+            }`}
+          >
+            Como você está se sentindo hoje?
+          </h1>
+          <p
+            className={`${
+              darkTheme ? "text-muted-dark" : "text-muted-light"
+            } text-sm`}
+          >
+            Registre seu humor e fatores que influenciam seu bem-estar
+          </p>
+        </div>
+
+        <MoodSelection
+          selectedMood={selectedMood}
+          setSelectedMood={setSelectedMood}
+        />
+        <EnergyLevelSelector
+          energyLevel={energyLevel}
+          setEnergyLevel={setEnergyLevel}
+        />
+        <FactorsSelection
+          selectedFactors={selectedFactors}
+          setSelectedFactors={setSelectedFactors}
+        />
+        <PersonalNotes
+          personalNotes={personalNotes}
+          setPersonalNotes={setPersonalNotes}
+        />
+
+        <div className="flex justify-center">
+          <button className="text-primary-dark w-full max-w-2xl cursor-pointer rounded-xl bg-blue-500 px-5 py-2 font-medium">
+            Salvar Registro do Dia
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MoodRegisterForm;
---
+++ src/pages/MoodMeasurer/interfaces.ts
@@ -0,0 +1,12 @@
+export interface MoodOption {
+  id: number;
+  label: string;
+  icon: string;
+  color: string;
+}
+
+export interface Factor {
+  id: number;
+  label: string;
+  icon: string;
+}
