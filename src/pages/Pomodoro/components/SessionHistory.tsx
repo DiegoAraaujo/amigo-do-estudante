ed124ffd3d014e5c05cf5c6bc02e80d6d54c0335
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 17:08:56 -0300

    feat(SessionHistory): create session history component displaying recent study sessions

---
+++ src/pages/Pomodoro/components/SessionHistory.tsx
@@ -0,0 +1,51 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+import SessionCard from "./SessionCard";
+
+const SessionHistory = () => {
+  const { darkTheme } = useDarkTheme();
+  const sessions = [
+    {
+      subject: "Matematica",
+      time: 1500,
+    },
+    {
+      subject: "Matematica",
+      time: 1800,
+    },
+  ];
+
+  return (
+    <div
+      className={`${darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"} flex-1 rounded-xl p-6`}
+    >
+      <p
+        className={`${darkTheme ? "text-primary-dark" : "text-primary-light"} sm:text-2xlfont-semibold text-xl`}
+      >
+        Últimas Sessões
+      </p>
+      <div className="flex items-center justify-center p-6">
+        {sessions.length > 0 ? (
+          <div className="flex flex-col gap-6 sm:flex-row lg:flex-col">
+            {sessions.map((session, index) => {
+              return (
+                <SessionCard
+                  subject={session.subject}
+                  time={session.time}
+                  key={index}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <p
+            className={`${darkTheme ? "text-muted-dark" : "text-muted-light"}`}
+          >
+            Nenhuma sessão ainda
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SessionHistory;
