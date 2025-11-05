facd6ea6cd119e4fe3cb95496e4d0b19ce19e41d
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 16:41:38 -0300

    feat(MoodSelection): create component to select and display current mood

---
+++ src/pages/MoodMeasurer/components/MoodSelection.tsx
@@ -0,0 +1,44 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+import MoodSelector from "./MoodSelector";
+import moodOptions from "../../../data/moodOptions";
+import type { MoodOption } from "../../interfaces";
+import type React from "react";
+
+interface MoodSelectionProps {
+  selectedMood: MoodOption | null;
+  setSelectedMood: React.Dispatch<React.SetStateAction<MoodOption | null>>;
+}
+
+const MoodSelection = ({
+  selectedMood,
+  setSelectedMood,
+}: MoodSelectionProps) => {
+  const { darkTheme } = useDarkTheme();
+
+  return (
+    <div className="mb-6 flex flex-col">
+      <h2
+        className={`mb-3 text-sm font-medium ${
+          darkTheme ? "text-primary-dark" : "text-primary-light"
+        }`}
+      >
+        Escolha seu humor
+      </h2>
+      <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4">
+        {moodOptions.map((mood) => (
+          <MoodSelector
+            key={mood.id}
+            id={mood.id}
+            label={mood.label}
+            icon={mood.icon}
+            color={mood.color}
+            moodSelected={selectedMood}
+            setMoodSelected={setSelectedMood}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MoodSelection;
