54cf7dc73d8a2a97f90d016ec1ceec7946e13d3a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-05 16:58:16 -0300

    feat(StudySelector): create component for selecting study focus or task category

---
+++ src/pages/Pomodoro/components/StudySelector.tsx
@@ -0,0 +1,43 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+const StudySelector = () => {
+  const { darkTheme } = useDarkTheme();
+  return (
+    <div className="flex w-full max-w-96 flex-col items-center gap-3">
+      <p
+        className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-sm font-medium`}
+      >
+        Matéria de Estudo
+      </p>
+      <select
+        name=""
+        id=""
+        className={`${darkTheme ? "text-primary-dark bg-dark" : "text-primary-light bg-card-light"} w-full rounded-xl px-6 py-2 text-sm`}
+      >
+        <option value="" disabled selected hidden className="text-sm">
+          Selecione uma Materia
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+        <option value="" className="text-sm">
+          Matematica
+        </option>
+      </select>
+    </div>
+  );
+};
+
+export default StudySelector;
