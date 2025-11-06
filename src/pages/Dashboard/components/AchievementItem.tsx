eeb8fd28353a4f9d9ae8880c8fc2422df33ce516
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-06 16:07:50 -0300

    feat(AchievementItem): develop AchievementItem component to display user achievements with XP details

---
+++ src/pages/Dashboard/components/AchievementItem.tsx
@@ -0,0 +1,61 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+interface AchievementItemProps {
+  conquest: string;
+  description: string;
+  xp: number;
+  icon: string;
+}
+const AchievementItem = ({
+  conquest,
+  description,
+  xp,
+  icon,
+}: AchievementItemProps) => {
+  const { darkTheme } = useDarkTheme();
+
+  return (
+    <div
+      className={`${
+        darkTheme ? "bg-dark" : "bg-light"
+      } flex items-center justify-between rounded-xl px-5 py-2`}
+    >
+      <div className="flex items-center gap-3">
+        <div
+          className={`rounded-full ${
+            darkTheme ? "bg-card-dark" : "bg-card-light"
+          }  px-3 py-2`}
+        >
+          <i className={`${icon} text-[16px] text-blue-900`} />
+        </div>
+        <div>
+          <p
+            className={`text-sm font-medium ${
+              darkTheme ? "text-primary-dark" : "text-primary-light"
+            }`}
+          >
+            {conquest}
+          </p>
+          <p
+            className={`text-xs ${
+              darkTheme ? "text-muted-dark" : "text-muted-light"
+            }`}
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+      <div
+        className={`rounded-2xl  ${
+          darkTheme
+            ? "bg-card-dark text-muted-dark"
+            : " bg-card-light text-muted-light"
+        }`}
+      >
+        <p className="px-2 py-1 text-xs font-medium">+{xp} XP</p>
+      </div>
+    </div>
+  );
+};
+
+export default AchievementItem;
