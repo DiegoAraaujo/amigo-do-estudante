ee9c70e27350339ab1bbf3a95cec0d6a394b15b0
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-06 16:08:42 -0300

    feat(ProgressItem): develop ProgressItem component to display Pomodoro goal progress on dashboard

---
+++ src/pages/Dashboard/components/ProgressItem.tsx
@@ -0,0 +1,28 @@
+import ProgressBar from "../../../components/ProgressBar";
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+const ProgressItem = () => {
+  const { darkTheme } = useDarkTheme();
+  return (
+    <div className="flex flex-col items-center gap-3 py-2 px-5 ">
+      <p className="text-3xl font-bold text-[#875AF2]">62%</p>
+      <p
+        className={`text-sm ${
+          darkTheme ? "text-muted-dark" : "text-muted-light"
+        }`}
+      >
+        Meta de Pomodoros
+      </p>
+      <ProgressBar percentage={50} />
+      <p
+        className={`text-sm ${
+          darkTheme ? "text-muted-dark" : "text-muted-light"
+        }`}
+      >
+        5 de 8
+      </p>
+    </div>
+  );
+};
+
+export default ProgressItem;
