f0f2a0d14dc7b7cc9f17bf08a0255399c2cdc61f
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-06 16:15:39 -0300

    feat(App): configure main routes and global providers for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DarkContextProvider } from "./context/DarkTheme";
+import { PomodoroProvider } from "./context/Pomodoro";
+
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import MoodMeasurer from "./pages/MoodMeasurer";
+import Container from "./components/Container";
+import Pomodoro from "./pages/Pomodoro";
+import Community from "./pages/Community";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <DarkContextProvider>
+      <Container>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route
+              path="/pomodoro/:subpage?"
+              element={
+                <PomodoroProvider>
+                  <Pomodoro />
+                </PomodoroProvider>
+              }
+            />
+            <Route path="/community/:subpage?" element={<Community />} />
+            <Route path="/:period/:subpage" element={<Dashboard />} />
+            <Route path="/moodmeasurer/:subpage?" element={<MoodMeasurer />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </DarkContextProvider>
+  );
+};
 
-export default App
+export default App;
