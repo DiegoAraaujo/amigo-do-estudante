50acbf8321b7d6f798fc57f1a38750cfe0aaedfc
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-06 19:29:09 -0300

    feat(ProfileFormActions): implement edit and save actions for profile form

---
+++ src/pages/Profile/components/ProfileFormActions.tsx
@@ -0,0 +1,59 @@
+import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+
+interface ProfileFormActionsProps {
+  editMode: boolean;
+  setEditMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ProfileFormActions = ({
+  editMode,
+  setEditMode,
+}: ProfileFormActionsProps) => {
+  const { darkTheme } = useDarkTheme();
+  return (
+    <div>
+      {editMode ? (
+        <div className="flex gap-3">
+          <button
+            className={`px-8 py-1 rounded-lg cursor-pointer ${
+              darkTheme
+                ? "bg-dark text-primary-dark"
+                : "bg-light text-primary-light"
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              setEditMode(false);
+            }}
+          >
+            Cancelar
+          </button>
+          <button
+            className={`px-8 py-1 rounded-lg cursor-pointer ${
+              darkTheme
+                ? "bg-blue-600 text-primary-dark"
+                : "bg-light text-primary-light"
+            }`}
+          >
+            Salvar
+          </button>
+        </div>
+      ) : (
+        <button
+          className={`px-8 py-1 rounded-lg cursor-pointer ${
+            darkTheme
+              ? "bg-dark text-primary-dark"
+              : "bg-light text-primary-light"
+          }`}
+          onClick={(e) => {
+            e.preventDefault();
+            setEditMode(true);
+          }}
+        >
+          Editar
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ProfileFormActions;
