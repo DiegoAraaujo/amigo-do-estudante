964ce588975a3e194a8fe2baa6f43bc63064a5f0
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-10 08:14:45 -0300

    style(QuickActions): adjust layout to use flex display for better alignment and responsiveness

--- src/pages/Community/components/QuickActions.tsx
+++ src/pages/Community/components/QuickActions.tsx
@@ -8,38 +8,66 @@ const QuickActions = ({ selectedSubpage }: QuickActionsProps) => {
   const { darkTheme } = useDarkTheme();
   return (
     <div
-      className={`${darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"} rounded-xl p-6 sm:flex-1 lg:flex-none`}
+      className={`${
+        darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"
+      } rounded-xl p-6 sm:flex-1 lg:flex-none `}
     >
       <h2
-        className={`text-xl font-semibold md:text-2xl ${darkTheme ? "text-primary-dark" : "text-primary-light"} mb-6`}
+        className={`text-xl font-semibold md:text-2xl ${
+          darkTheme ? "text-primary-dark" : "text-primary-light"
+        } mb-6`}
       >
         Ações Rápidas
       </h2>
       <div className="grid gap-3">
         <Link
           to="/community/allquestions"
-          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${selectedSubpage === "allquestions" ? "bg-blue-600" : darkTheme ? "bg-dark" : "bg-light text-primary-light"}`}
+          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${
+            selectedSubpage === "allquestions"
+              ? "bg-blue-600"
+              : darkTheme
+              ? "bg-dark"
+              : "bg-light text-primary-light"
+          }`}
         >
           <i className="bi bi-list-nested" />
           Todas as Pergunta
         </Link>
         <Link
           to="/community/createquestion"
-          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${selectedSubpage === "createquestion" ? "bg-blue-600" : darkTheme ? "bg-dark" : "bg-light text-primary-light"}`}
+          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${
+            selectedSubpage === "createquestion"
+              ? "bg-blue-600"
+              : darkTheme
+              ? "bg-dark"
+              : "bg-light text-primary-light"
+          }`}
         >
           <i className="bi bi-plus-lg" />
           Fazer Pergunta
         </Link>
         <Link
           to="/community/myquestions"
-          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${selectedSubpage === "myquestions" ? "bg-blue-600" : darkTheme ? "bg-dark" : "bg-light text-primary-light"}`}
+          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${
+            selectedSubpage === "myquestions"
+              ? "bg-blue-600"
+              : darkTheme
+              ? "bg-dark"
+              : "bg-light text-primary-light"
+          }`}
         >
           <i className="bi bi-chat-dots" />
           Minhas Perguntas
         </Link>
         <Link
           to="/community/myanswers"
-          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${selectedSubpage === "myanswers" ? "bg-blue-600" : darkTheme ? "bg-dark" : "bg-light text-primary-light"}`}
+          className={`text-primary-dark flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-3 text-sm font-medium ${
+            selectedSubpage === "myanswers"
+              ? "bg-blue-600"
+              : darkTheme
+              ? "bg-dark"
+              : "bg-light text-primary-light"
+          }`}
         >
           <i className="bi bi-ui-checks" />
           Minhas Respostas
