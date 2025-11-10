692bea5fd66ec775bb87f8209165369a5da62a2f
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-10 08:34:58 -0300

    feat(UserDropdown): hide dropdown when clicking on an option

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import { useDarkTheme } from "../hooks/UseDarkTheme";
+import type React from "react";
 
-
-const UserDropdown = () => {
+interface UserDropdownProps {
+  setDropDownVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const UserDropdown = ({ setDropDownVisible }: UserDropdownProps) => {
   const { darkTheme } = useDarkTheme();
 
   return (
     <div
-      className={`${
-        darkTheme ? "bg-dark" : "bg-light"
+      className={`border z-50 ${
+        darkTheme ? "bg-dark border-gray-500" : "bg-light border-gray-300"
       } flex flex-col rounded-xl p-3 w-60 absolute right-0 gap-2`}
     >
       <div
@@ -33,6 +36,9 @@ const UserDropdown = () => {
       </div>
       <Link
         to="/profile"
+        onClick={() => {
+          setDropDownVisible(false);
+        }}
         className={`cursor-pointer text-sm  flex gap-3 ${
           darkTheme ? "text-primary-dark" : "text-primary-light"
         }`}
@@ -40,7 +46,12 @@ const UserDropdown = () => {
         <i className="bi bi-person" />
         Perfil
       </Link>
-      <button className={`cursor-pointer text-sm flex gap-3 text-red-500`}>
+      <button
+        className={`cursor-pointer text-sm flex gap-3 text-red-500`}
+        onClick={() => {
+          setDropDownVisible(false);
+        }}
+      >
         <i className="bi bi-box-arrow-right" />
         Sair
       </button>
