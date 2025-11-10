366e4cb93c09b0415e7804e9f1425d94f57e77d5
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-10 08:15:33 -0300

    style(TopUsers): adjust layout structure using flex for better alignment

--- src/pages/Community/components/TopUsers.tsx
+++ src/pages/Community/components/TopUsers.tsx
@@ -5,33 +5,47 @@ const TopUsers = () => {
 
   return (
     <div
-      className={`${darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"} rounded-xl p-6 sm:flex-1 lg:flex-none`}
+      className={`${
+        darkTheme ? "bg-surface-dark" : "bg-surface-light shadow-sm"
+      } rounded-xl p-6 sm:flex-1 lg:flex-none`}
     >
       <h2
-        className={`text-xl font-semibold sm:text-2xl md:text-2xl ${darkTheme ? "text-primary-dark" : "text-primary-light"} mb-6`}
+        className={`text-xl font-semibold sm:text-2xl md:text-2xl ${
+          darkTheme ? "text-primary-dark" : "text-primary-light"
+        } mb-6`}
       >
         Top Usuários
       </h2>
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-3">
           <p
-            className={`${darkTheme ? "text-blue-600" : "text-primary-light"} text-lg font-bold`}
+            className={`${
+              darkTheme ? "text-blue-600" : "text-primary-light"
+            } text-lg font-bold`}
           >
             #1
           </p>
           <div
-            className={`${darkTheme ? "bg-card-dark text-muted-dark" : "bg-card-light text-muted-light"} flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
+            className={`${
+              darkTheme
+                ? "bg-card-dark text-muted-dark"
+                : "bg-card-light text-muted-light"
+            } flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
           >
             <i className="bi bi-person-circle"></i>
           </div>
           <div className="flex flex-col gap-0.5">
             <p
-              className={`${darkTheme ? "text-primary-dark" : "text-primary-light"} text-sm font-medium`}
+              className={`${
+                darkTheme ? "text-primary-dark" : "text-primary-light"
+              } text-sm font-medium`}
             >
               Prof. Carlos
             </p>
             <p
-              className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-xs`}
+              className={`${
+                darkTheme ? "text-muted-dark" : "text-muted-light"
+              } text-xs`}
             >
               Nível 8 • 2450 pts
             </p>
@@ -39,23 +53,33 @@ const TopUsers = () => {
         </div>
         <div className="flex items-center gap-3">
           <p
-            className={`${darkTheme ? "text-blue-600" : "text-primary-light"} text-lg font-bold`}
+            className={`${
+              darkTheme ? "text-blue-600" : "text-primary-light"
+            } text-lg font-bold`}
           >
             #1
           </p>{" "}
           <div
-            className={`${darkTheme ? "bg-card-dark text-muted-dark" : "bg-card-light text-muted-light"} flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
+            className={`${
+              darkTheme
+                ? "bg-card-dark text-muted-dark"
+                : "bg-card-light text-muted-light"
+            } flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
           >
             <i className="bi bi-person-circle"></i>
           </div>
           <div className="flex flex-col gap-0.5">
             <p
-              className={`${darkTheme ? "text-primary-dark" : "text-primary-light"} text-sm font-medium`}
+              className={`${
+                darkTheme ? "text-primary-dark" : "text-primary-light"
+              } text-sm font-medium`}
             >
               Prof. Carlos
             </p>
             <p
-              className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-xs`}
+              className={`${
+                darkTheme ? "text-muted-dark" : "text-muted-light"
+              } text-xs`}
             >
               Nível 8 • 2450 pts
             </p>
@@ -63,23 +87,33 @@ const TopUsers = () => {
         </div>
         <div className="flex items-center gap-3">
           <p
-            className={`${darkTheme ? "text-blue-600" : "text-primary-light"} text-lg font-bold`}
+            className={`${
+              darkTheme ? "text-blue-600" : "text-primary-light"
+            } text-lg font-bold`}
           >
             #1
           </p>{" "}
           <div
-            className={`${darkTheme ? "bg-card-dark text-muted-dark" : "bg-card-light text-muted-light"} flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
+            className={`${
+              darkTheme
+                ? "bg-card-dark text-muted-dark"
+                : "bg-card-light text-muted-light"
+            } flex h-9 w-9 items-center justify-center rounded-full text-2xl`}
           >
             <i className="bi bi-person-circle"></i>
           </div>
           <div className="flex flex-col gap-0.5">
             <p
-              className={`${darkTheme ? "text-primary-dark" : "text-primary-light"} text-sm font-medium`}
+              className={`${
+                darkTheme ? "text-primary-dark" : "text-primary-light"
+              } text-sm font-medium`}
             >
               Prof. Carlos
             </p>
             <p
-              className={`${darkTheme ? "text-muted-dark" : "text-muted-light"} text-xs`}
+              className={`${
+                darkTheme ? "text-muted-dark" : "text-muted-light"
+              } text-xs`}
             >
               Nível 8 • 2450 pts
             </p>
