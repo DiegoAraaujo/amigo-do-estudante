616ccbf8a62fc79a836c26a9d6cb2fdccac2ccda
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-11 09:23:33 -0300

    style(PersonalNotes): adjust input borders and remove focus outline

--- src/pages/MoodMeasurer/components/PersonalNotes.tsx
+++ src/pages/MoodMeasurer/components/PersonalNotes.tsx
@@ -13,12 +13,18 @@ const PersonalNotes = ({
   return (
     <div className="mb-6">
       <h2
-        className={`text-sm mb-3 font-medium ${darkTheme ? "text-primary-dark" : "text-primary-light"}`}
+        className={`text-sm mb-3 font-medium ${
+          darkTheme ? "text-primary-dark" : "text-primary-light"
+        }`}
       >
         Notas pessoais (opcional)
       </h2>
       <textarea
-        className={`h-30 w-full rounded-xl border border-gray-300 p-6 focus:border-[#875AF2] ${darkTheme ? "bg-dark text-primary-dark" : "bg-light"} text-xs tracking-wider`}
+        className={`h-30 w-full focus:outline-none rounded-xl border border-gray-300 p-6 ${
+          darkTheme
+            ? "bg-dark text-primary-dark border-outline-dark"
+            : "border-outline-light bg-light"
+        } text-xs tracking-wider`}
         placeholder="Como foi o seu dia? Oque você aprendeu sobre si mesmo?"
         value={personalNotes}
         onChange={(e) => {
