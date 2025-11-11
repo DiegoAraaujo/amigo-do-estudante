1005ecaa693f0f08a37878e93aac930acac07404
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-11 09:24:03 -0300

    style(MoodRecordCard): add border styles for dark and light themes

--- src/pages/MoodMeasurer/components/MoodRecordCard.tsx
+++ src/pages/MoodMeasurer/components/MoodRecordCard.tsx
@@ -13,7 +13,11 @@ const MoodRecordCard = ({ date, note, mood, factors }: MoodRecordCardProps) => {
 
   return (
     <div
-      className={`flex flex-col gap-4 rounded-xl p-5 ${darkTheme ? "bg-card-dark" : "bg-card-light"}`}
+      className={`flex flex-col gap-4 rounded-xl p-6 border ${
+        darkTheme
+          ? "bg-card-dark border-outline-dark"
+          : "bg-card-light border-outline-light"
+      }`}
     >
       <div className="flex items-center gap-4">
         <div className="flex h-10 w-10 items-center justify-center rounded-full bg-green-300">
@@ -21,19 +25,25 @@ const MoodRecordCard = ({ date, note, mood, factors }: MoodRecordCardProps) => {
         </div>
         <div>
           <p
-            className={`text-base font-medium sm:text-lg ${darkTheme ? "text-primary-dark" : "text-primary-light"}`}
+            className={`text-base font-medium sm:text-lg ${
+              darkTheme ? "text-primary-dark" : "text-primary-light"
+            }`}
           >
             {mood}
           </p>
           <p
-            className={`text-sm ${darkTheme ? "text-muted-dark" : "text-muted-light"}`}
+            className={`text-sm ${
+              darkTheme ? "text-muted-dark" : "text-muted-light"
+            }`}
           >
             {date}
           </p>
         </div>
       </div>
       <p
-        className={`rounded-xl p-3 text-sm shadow-xs ${darkTheme ? "bg-dark text-muted-dark" : "bg-light text-muted-light"}`}
+        className={`rounded-xl p-3 text-sm shadow-xs ${
+          darkTheme ? "bg-dark text-muted-dark" : "bg-light text-muted-light"
+        }`}
       >
         "{note}"
       </p>
