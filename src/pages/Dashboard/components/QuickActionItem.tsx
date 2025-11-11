c99d21c31f007ba81c6a8a0f86914d62d2ac9370
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-11 16:43:40 -0300

    feat(QuickActionItem): make Link functional to navigate to target path

--- src/pages/Dashboard/components/QuickActionItem.tsx
+++ src/pages/Dashboard/components/QuickActionItem.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useDarkTheme } from "../../../hooks/UseDarkTheme";
+import { Link } from "react-router-dom";
 
 interface QuickActionItemProps {
   action: string;
   icon: string;
   hoverColor: string;
+  path: string;
 }
 
 const QuickActionItem = ({
   action,
   icon,
   hoverColor,
+  path,
 }: QuickActionItemProps) => {
   const [hover, setHover] = React.useState(false);
   const { darkTheme } = useDarkTheme();
 
   return (
-    <div
+    <Link
+      to={`${path}`}
       className={`flex cursor-pointer justify-between rounded-xl   py-2 px-4 transition-colors border group ${
         darkTheme
           ? "bg-card-dark  border-outline-dark"
@@ -41,7 +45,7 @@ const QuickActionItem = ({
           darkTheme ? "text-primary-dark" : "text-primary-light"
         } bi bi-arrow-right text-[16px] group-hover:text-primary-dark group-hover:translate-x-3  transition-translate duration-300`}
       ></i>
-    </div>
+    </Link>
   );
 };
 
