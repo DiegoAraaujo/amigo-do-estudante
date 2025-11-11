681d43cbf0cc9882bb2b0f5429398ae576f8f035
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-11-11 08:44:57 -0300

    feat: create Materials page with material listing and add material modal

---
+++ src/pages/Materials/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useDarkTheme } from "../../hooks/UseDarkTheme";
+import MaterialCard from "./components/MaterialCard";
+import AddMaterialModal from "./components/AddMaterialModal";
+
+const Materials = () => {
+  const { darkTheme } = useDarkTheme();
+  const [isAddMaterialModalOpen, setIsAddMaterialModalOpen] =
+    useState<boolean>(false);
+
+  return (
+    <main>
+      {isAddMaterialModalOpen && (
+        <AddMaterialModal
+          setIsAddMaterialModalOpen={setIsAddMaterialModalOpen}
+        />
+      )}
+      <div className="flex justify-end mb-6">
+        <button
+          className="bg-blue-500 text-primary-dark max-w-50 w-full px-6 py-2 rounded-lg font-semibold cursor-pointer"
+          onClick={() => setIsAddMaterialModalOpen(true)}
+        >
+          Adicionar Material
+        </button>
+      </div>
+      <section
+        className={`gap-4 grid lg:grid-cols-2 p-6 rounded-xl ${
+          darkTheme ? "bg-surface-dark" : "bg-surface-light rounded-xl"
+        }`}
+      >
+        <MaterialCard />
+        <MaterialCard />
+      </section>
+    </main>
+  );
+};
+
+export default Materials;
